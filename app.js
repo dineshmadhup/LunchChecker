0d018832dd30e6bf811be9e3dee9b9d1d84185a8
dineshmadhup <dineshmadhup@gmail.com>
2016-09-16 15:53:02 -0700

    Files added

---
+++ app.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+    angular.module("LunchCheck", ['ngMessages'])
+        .controller("LunchCheckController", LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.addItemList = function () {
+            var list = $scope.foodItem;
+            //$scope.foodCount = $scope.foodItem.split(',').length;
+            var foodCount = list.length;
+            if (foodCount <= 3) {
+                $scope.enjoyormuch = "Enjoy";
+            } else if (foodCount > 3) {
+                $scope.enjoyormuch = "Too Much";
+            }
+        };
+    }
+})();
